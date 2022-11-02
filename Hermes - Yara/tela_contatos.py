258cd0f3357159169e6ac9de62955de6e03f3f81
Yara Rodrigues Inácio <yara.rodrigues.inacio@gmail.com>
2022-11-02 10:24:17 -0300

    Add files via upload

---
+++ Hermes - Yara/tela_contatos.py
@@ -0,0 +1,7 @@
+import os
+os.system("cls")
+
+def tela_contatos():
+    print("\nTelefone: +55 (82) 3325-7785\n\nInstagram: <https://www.instagram.com/chambreshoteis/>\n\nFacebook: <https://www.facebook.com/chambreshoteis>\n")
+
+tela_contatos()
---
+++ Hermes - Yara/voltar.py
@@ -0,0 +1,10 @@
+import os
+os.system("cls")
+
+def menu():
+    print("Menu")
+
+def voltar():
+    menu()
+
+voltar()
